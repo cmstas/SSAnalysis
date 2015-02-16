6e4fc15ac48d97bdd6625385ecc4eb3b7a31e14d
Giuseppe Cerati <giuseppe.cerati@cern.ch>
2015-02-16 11:36:05 +0100

    plot lep eff vs ht

--- macros/plotEff.C
+++ macros/plotEff.C
@@ -7,26 +7,37 @@
   TString dir = "results_PHYS14";
 
   TString sample = "dy";
-  //sample = "T1ttttG1500";
+  //sample = "T1ttttG1200";
   //sample = "T5Full1200";
   //sample = "TTWJets";
 
+  bool doht = false;
   bool dofo = false;
+
+  TString prefix = "";
+  TString postfix = "";
+  if (doht)  prefix  = "_ht";
+  if (dofo)  postfix = "_vlnoiso";
 
   TFile* f = TFile::Open(dir+"/"+sample+"_histos_effic.root");
 
-  TH2F* mud = (TH2F*) f->Get("ef_mu_den");
-  TH2F* mun = (TH2F*) f->Get(dofo ? "ef_mu_num_fo" :  "ef_mu_num");
+  TH2F* mud = (TH2F*) f->Get("ef"+prefix+"_mu_den");
+  mud->GetXaxis()->SetTitle("p_{T} [GeV]");
+  mud->GetYaxis()->SetTitle("|#eta|");
+  if (doht) {
+    mud->GetYaxis()->SetTitle("H_{T} [GeV]");
+    mud->GetXaxis()->SetNdivisions(5,0);
+    mud->GetYaxis()->SetNdivisions(6,0);
+  }
+  mud->GetXaxis()->SetTitleSize(0.05);
+  mud->GetYaxis()->SetTitleSize(0.05);
+  mud->GetXaxis()->SetTitleOffset(0.85);
+  mud->GetYaxis()->SetTitleOffset(0.8);
+  TH2F* mun = (TH2F*) f->Get("ef"+prefix+"_mu_num"+postfix);
   TH2F* muf = (TH2F*) mud->Clone("ef_mu");
   muf->Reset();
   if (!muf->GetSumw2N()) muf->Sumw2();
   muf->SetTitle("muon efficiency");
-  muf->GetXaxis()->SetTitle("p_{T} [GeV]");
-  muf->GetYaxis()->SetTitle("|#eta|");
-  muf->GetXaxis()->SetTitleSize(0.05);
-  muf->GetYaxis()->SetTitleSize(0.05);
-  muf->GetXaxis()->SetTitleOffset(0.85);
-  muf->GetYaxis()->SetTitleOffset(0.8);
   muf->Divide(mun,mud,1.0,1.0,"B");
   /*
   for (int i=1;i<=muf->GetXaxis()->GetNbins();++i) {
@@ -44,19 +55,28 @@
   muf->SetMarkerSize(1.5);
   muf->Draw("texte,colz");
   c1.RedrawAxis();
+  TCanvas c1_1;
+  mud->SetMarkerSize(1.5);
+  mud->Draw("texte,colz");
+  c1_1.RedrawAxis();
 
-  TH2F* eld = (TH2F*) f->Get("ef_el_den");
-  TH2F* eln = (TH2F*) f->Get(dofo ? "ef_el_num_fo" : "ef_el_num");
+  TH2F* eld = (TH2F*) f->Get("ef"+prefix+"_el_den");
+  eld->GetXaxis()->SetTitle("p_{T} [GeV]");
+  eld->GetYaxis()->SetTitle("|#eta|");
+  if (doht) {
+    eld->GetYaxis()->SetTitle("H_{T} [GeV]");
+    eld->GetXaxis()->SetNdivisions(5,0);
+    eld->GetYaxis()->SetNdivisions(6,0);
+  }
+  eld->GetXaxis()->SetTitleSize(0.05);
+  eld->GetYaxis()->SetTitleSize(0.05);
+  eld->GetXaxis()->SetTitleOffset(0.85);
+  eld->GetYaxis()->SetTitleOffset(0.8);
+  TH2F* eln = (TH2F*) f->Get("ef"+prefix+"_el_num"+postfix);
   TH2F* elf = (TH2F*) eld->Clone("ef_el");
   elf->Reset();
   if (!elf->GetSumw2N()) elf->Sumw2();
   elf->SetTitle("electron efficiency");
-  elf->GetXaxis()->SetTitle("p_{T} [GeV]");
-  elf->GetYaxis()->SetTitle("|#eta|");
-  elf->GetXaxis()->SetTitleSize(0.05);
-  elf->GetYaxis()->SetTitleSize(0.05);
-  elf->GetXaxis()->SetTitleOffset(0.85);
-  elf->GetYaxis()->SetTitleOffset(0.8);
   elf->Divide(eln,eld,1.0,1.0,"B");
   /*
   for (int i=1;i<=elf->GetXaxis()->GetNbins();++i) {
@@ -74,13 +94,14 @@
   elf->SetMarkerSize(1.5);
   elf->Draw("texte,colz");
   c2.RedrawAxis();
+  TCanvas c2_1;
+  eld->SetMarkerSize(1.5);
+  eld->Draw("texte,colz");
+  c2_1.RedrawAxis();
 
-  if (dofo) {
-    c1.SaveAs(dir+"/"+sample+"_mu_ef_fo.png");
-    c2.SaveAs(dir+"/"+sample+"_el_ef_fo.png");
-  } else {
-    c1.SaveAs(dir+"/"+sample+"_mu_ef.png");
-    c2.SaveAs(dir+"/"+sample+"_el_ef.png");
-  }
+  c1.SaveAs(dir+"/"+sample+"_mu_ef"+prefix+postfix+".png");
+  c2.SaveAs(dir+"/"+sample+"_el_ef"+prefix+postfix+".png");
+  c1_1.SaveAs(dir+"/"+sample+"_mu_ef"+prefix+postfix+"_den.png");
+  c2_1.SaveAs(dir+"/"+sample+"_el_ef"+prefix+postfix+"_den.png");
 
 }
