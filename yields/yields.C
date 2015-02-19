3fc827422381028fa50213c7f1b74e09073b2404
Christopher A. George <george@physics.ucsb.edu>
2015-02-19 14:17:03 -0800

    bug fix, add comments

--- yields/yields.C
+++ yields/yields.C
@@ -11,6 +11,7 @@
 
 using namespace std;
 
+//Enums
 enum sample_t { ALL, TTW, TTBAR, TTZ, WZ, TTBAR_8TEV, T1TTTT_1500_100, T1TTTT_1200_800, TEST };
 enum selection_t { SS, OS };
 enum weight_t { WEIGHTED, UNWEIGHTED };
@@ -20,93 +21,128 @@ enum mtmin_t { NONE, MT100, MT125, MT150 };
 enum hyp_class_t { INC1, INC2, NORMAL };
 enum pt_t { HIHI, HILO, LOLO };
 
-//switches
-char* path = "../babymaker/v1.03";
-char* output_dir = "mydir"; 
-int nRegions = 1;
-bool scan = 0; 
-int scanRegion = 0;
-bool doMC = 0;
-mtmin_t mt_min = NONE;
-sample_t sample = WZ;
-selection_t selection = SS;
-weight_t weighted = WEIGHTED;
-region_t regions = NEW;
-lep_t leps = ANY;
-char* testfile = "ttbar.root";
-hyp_class_t hyp_class_wanted = NORMAL;
-bool makeHisto = true;
-int met_cut = 120; //120 for 8 TeV 
-int ht_cut = 400;  //400 for 8 TeV
-int njets_cut = 4; //4 for 8 TeV
+//Switches
+char* path = "../babymaker/v1.03";     //where the data is
+char* output_dir = "mydir";            //dir where plots should go (in parent directory, will create if doesn't exist)
+int nRegions = 1;                      //which regions to include (can be single region, < 10 will turn off b-tagging)
+int firstSR = 0;                       //first SR you want to consider (usually 0 or 1)
+bool scan = 0;                         //make printout of events
+int scanRegion = 0;                    //only needed if scan == TRUE
+mtmin_t mt_min = NONE;                 //add mt_min cut
+sample_t sample = WZ;                  //Sample as defined above
+selection_t selection = SS;            //select SS or OS
+weight_t weighted = WEIGHTED;          //weight by cross-section or not
+lep_t leps = ANY;                      //segregate by flavor composition (MU, EL, EM, ANY)
+char* testfile = "ttbar.root";         //only needed if sample is TEST
+hyp_class_t hyp_class_wanted = NORMAL; //INC1 is inclusive SS, INC2 is h-h or h-l SS, NORMAL is h-h SS only
+region_t regions = NEW;                //new pT regions (25/10) or old (10/10)
+int met_cut = 120;                     //120 for 8 TeV 
+int ht_cut = 400;                      //400 for 8 TeV
+int njets_cut = 4;                     //4 for 8 TeV
+int btag_pt = 40;                      //40 for 8 TeV
+bool doBtagging = true;                //should SRs have b-tagging applied (automatically true for nRegions > 10)
+bool doTruthMatching = true;           //require truth-level leptons to be matched to correctly-signed (SS or OS) pair
 
+//Declare output file
 TFile *file;
 
 vector<float> doIt(TTree *chain, TCut cuts, pt_t pt_){
 
+  //Declare and create output histogram
   TH1F *hist; 
   if (pt_ == HIHI) hist = new TH1F("hyp_hihi", "hyp_hihi", nRegions, 1, nRegions+1);
   if (pt_ == HILO) hist = new TH1F("hyp_hilow", "hyp_hilow", nRegions, 1, nRegions+1);
   if (pt_ == LOLO) hist = new TH1F("hyp_lowlow", "hyp_lowlow", nRegions, 1, nRegions+1);
 
+  //Create vector with yields to return
   vector <float> temp;
 
+  //-------CUTS-------
+  //Void, initialize baseline
+  TCut none("1.0");
+  TCut baseline = none;
+  
+  //Weight
   TCut weight_13("scale1fb*10.0");
   TCut notweighted("1.0");
   TCut weight = (weighted == WEIGHTED) ? weight_13 : notweighted;
   TCut weight8("1.95*sf_dilepTrig_hpt*sf_dilep_eff");
   if (sample == TTBAR_8TEV) weight *= weight8;
+
+  //Hyp class (num-num, num-denom, etc.)
   TCut selection1("hyp_class == 1 || hyp_class == 2 || hyp_class == 3");
   TCut selection2("hyp_class == 2 || hyp_class == 3");
   TCut selectionSS("hyp_class == 3");
   TCut selectionOS("hyp_class == 4");
+  baseline += (hyp_class_wanted == NORMAL ? (selection == OS ? selectionOS : selectionSS) : none);
+  if (hyp_class_wanted == INC1) baseline += selection1; 
+  if (hyp_class_wanted == INC2) baseline += selection2; 
+
+  //Lep pT requirements
   TCut highLow("((lep1_p4.pt() > 10 && lep2_p4.pt() > 25) || (lep1_p4.pt() > 25 && lep2_p4.pt() > 10))"); 
   TCut lowLow("((lep1_p4.pt() > 10 && lep2_p4.pt() > 10) && (lep1_p4.pt() < 25 && lep2_p4.pt() < 25))"); 
   TCut high_new("lep1_p4.pt() > 25 && lep2_p4.pt() > 25"); 
   TCut high_old("lep1_p4.pt() > 20 && lep2_p4.pt() > 20"); 
+  TCut high = regions == NEW ? high_new : high_old;
+ 
+  //Truth matching
   TCut mc_ss("lep1_mc_id*lep2_mc_id > 0");
   TCut mc_os("lep1_mc_id*lep2_mc_id < 0");
-  TCut njets2("njets >= 2");
-  TCut high = regions == NEW ? high_new : high_old;
-  TCut baseline = njets2;
+  baseline += (selection == OS) ? mc_os : mc_ss;
   if (pt_ == HIHI) baseline += high;
   if (pt_ == HILO) baseline += highLow;
   if (pt_ == LOLO) baseline += lowLow;
-  TCut none("1.0");
-  baseline += (hyp_class_wanted == NORMAL ? (selection == OS ? selectionOS : selectionSS) : none);
-  baseline += (selection == OS) ? mc_os : mc_ss;
+ 
+  //nJets
+  TCut njets2("njets >= 2");
+  baseline += njets2;
+  TCut njets_low(Form("njets >= 2 && njets < %i", njets_cut));
+  TCut njets_high(Form("njets >= %i", njets_cut));
+
+  //Lepton flavor
   TCut el("abs(lep1_id) == 11 && abs(lep2_id) == 11");
   TCut mu("abs(lep1_id) == 13 && abs(lep2_id) == 13");
   TCut emu("(abs(lep1_id) == 11 && abs(lep2_id) == 13) || (abs(lep1_id) == 13 && abs(lep2_id) == 11)");
   if (leps == EL) baseline += el;
   if (leps == MU) baseline += mu;
   if (leps == EM) baseline += emu;
-  TCut njets_low(Form("njets >= 2 && njets < %i", njets_cut));
-  TCut njets_high(Form("njets >= %i", njets_cut));
+
+  //MET, HT
   TCut met_low(Form("met > 50 && met < %i", met_cut));
   TCut met_high(Form("met > %i", met_cut));
   TCut ht_low(Form("ht > 200 && ht < %i", ht_cut));
   TCut ht_high(Form("ht > %i", ht_cut));
-  TCut nbtagsi("nbtags >= 0");
-  TCut nbtags0("Sum$(jets.pt()>25 && jets_disc>0.814 ? 1 : 0) == 0");
-  TCut nbtags1("Sum$(jets.pt()>25 && jets_disc>0.814 ? 1 : 0) == 1");
-  TCut nbtags2("Sum$(jets.pt()>25 && jets_disc>0.814 ? 1 : 0) == 2");
-  TCut nbtags3p("Sum$(jets.pt()>25 && jets_disc>0.814 ? 1 : 0) >= 3");
   TCut htmet("ht > 500 ? 1 : met > 30");
+
+  //nBtags
+  TCut nbtags0_slow(Form("Sum$(jets.pt()>%i && jets_disc>0.814 ? 1 : 0) == 0", btag_pt));
+  TCut nbtags1_slow(Form("Sum$(jets.pt()>%i && jets_disc>0.814 ? 1 : 0) == 1", btag_pt));
+  TCut nbtags2_slow(Form("Sum$(jets.pt()>%i && jets_disc>0.814 ? 1 : 0) == 2", btag_pt));
+  TCut nbtags3p_slow(Form("Sum$(jets.pt()>%i && jets_disc>0.814 ? 1 : 0) >= 3", btag_pt));
+  TCut nbtags0_quick("njets == 0");
+  TCut nbtags1_quick("njets == 1");
+  TCut nbtags2_quick("njets == 2");
+  TCut nbtags3p_quick("njets >= 3");
+  TCut nbtags0 =  btag_pt == 40 ? nbtags0_quick  : nbtags0_slow;
+  TCut nbtags1 =  btag_pt == 40 ? nbtags1_quick  : nbtags1_slow;
+  TCut nbtags2 =  btag_pt == 40 ? nbtags2_quick  : nbtags2_slow;
+  TCut nbtags3p = btag_pt == 40 ? nbtags3p_quick : nbtags3p_slow;
+
+  //MT MIN
   TCut mtmin100("mt > mt_l2 ? mt_l2 > 100 : mt > 100");
   TCut mtmin125("mt > mt_l2 ? mt_l2 > 125 : mt > 125");
   TCut mtmin150("mt > mt_l2 ? mt_l2 > 150 : mt > 150");
   if (mt_min == MT100) baseline += mtmin100;
   if (mt_min == MT125) baseline += mtmin125;
   if (mt_min == MT150) baseline += mtmin150;
-  if (hyp_class_wanted == INC1) baseline += selection1; 
-  if (hyp_class_wanted == INC2) baseline += selection2; 
 
-  for (int i = 0; i < nRegions+1; i++){
-    if (i%10 == 9 || i%10 == 0){
+  //Define each SR, make yield, plot
+  for (int i = firstSR; i < nRegions+1; i++){
+    if (i%10 == 9){
       temp.push_back(-1);
       continue;
     }
+ 
     TH1F* SR = new TH1F("SR", "SR", 1, 0, 1);
     TCut cut;
     if (i%10 == 1) cut = met_low*ht_low*njets_low; 
@@ -117,7 +153,7 @@ vector<float> doIt(TTree *chain, TCut cuts, pt_t pt_){
     if (i%10 == 6) cut = met_high*ht_high*njets_low; 
     if (i%10 == 7) cut = met_high*ht_low*njets_high; 
     if (i%10 == 8) cut = met_high*ht_high*njets_high; 
-    if (nRegions > 10){
+    if (doBtagging || nRegions > 10){
       if (i-30 >= 0) cut *= nbtags3p;
       else if (i-20 >= 0) cut *= nbtags2;
       else if (i-10 >= 0) cut *= nbtags1;
@@ -144,7 +180,7 @@ vector<float> doIt(TTree *chain, TCut cuts, pt_t pt_){
 
 int yields(){
 
-  if (nRegions < 10) cout << "WARNING! Less than 10 SRs here!  Not doing any b-tag requirements" << endl;
+  if (nRegions < 10 && !doBtagging) cout << "WARNING! Not doing any b-tag requirements!" << endl;
   cout << Form("SR regions based around HT cut of %i, MET cut of %i, njets cut of %i", ht_cut, met_cut, njets_cut) << endl;
 
   //Check if directory exists; create it if not; otherwise, ask permission to overwrite it
@@ -161,14 +197,17 @@ int yields(){
   } 
   else system(Form("mkdir ../%s", output_dir)); 
 
+  //Warning if not running on all lepton flavors
   const char* ssoros = selection == SS ? "SS" : "OS";
   cout << "Running in " << ssoros << " mode" << endl;
   if (leps == EL) cout << "Running on dielectrons!" << endl; 
   if (leps == EM) cout << "Running on el-mus!" << endl; 
   if (leps == MU) cout << "Running on dimuons!" << endl; 
 
+  //Declare null TCut
   TCut none("1.0");
 
+  //Declare yield vectors
   vector <float> ttw_yields;
   vector <float> ttz_yields;
   vector <float> wz_yields;
@@ -179,16 +218,13 @@ int yields(){
   vector <float> t1tttt_2_yields;
   vector <float> t1tttt_3_yields;
 
+  //---------------For each sample, make yields, plots-------------------
   if (sample == TEST){
     TFile *test_file = new TFile(Form("../%s", testfile));
     TTree *test_tree = (TTree*)test_file->Get("t");
     test_yields = doIt(test_tree, none, HIHI);
     test_yields = doIt(test_tree, none, HILO);
     test_yields = doIt(test_tree, none, LOLO);
-  }
-
-  for (unsigned int i = 0; i < test_yields.size(); i++){
-    cout << i << " " << test_yields[i] << endl;
   }
 
   if (sample == ALL || sample == TTW){
