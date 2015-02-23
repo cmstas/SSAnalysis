8aa99b3aa92b30e37426d24d3f18d9e47a0acf08
Christopher A. George <george@physics.ucsb.edu>
2015-02-22 16:33:29 -0800

    updates

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 
 git clone git@github.com:cmstas/SSAnalysis.git
 cd SSAnalysis/
-git clone git@github.com:cmstas/CORE.git
-git clone git@github.com:cmstas/Tools.git
 export SCRAM_ARCH=slc6_amd64_gcc481
 cmsrel CMSSW_7_1_6
 cd CMSSW_7_1_6/src
@@ -15,3 +13,4 @@ git checkout v5.0.1
 scramv1 b clean
 scramv1 b -j 4
 cd ../../../../
+echo "IMPORTANT!  Set up symbolic link to your CORE and Tools dir with ln -s /path/to/CORE"
