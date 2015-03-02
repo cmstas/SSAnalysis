349b3530673c66a5507c8ad3d5161eb58e27ea74
Giuseppe <giuseppe.cerati@cern.ch>
2015-03-02 01:05:39 -0800

    uncomment stuff

--- FakeRate/measurement_region/doAll.C
+++ FakeRate/measurement_region/doAll.C
@@ -1,5 +1,5 @@
 {
-  string tag = "V00-00-05"; //CHANGE to 5 once they finish!!!
+  string tag = "V00-00-05";
 
   gROOT->ProcessLine(".L ScanChain.C+");
 
@@ -8,22 +8,22 @@
   qcd->Add( Form("../fake_rate_output/%s/qcd_EMEnriched.root",tag.c_str()) );
 
   ScanChain(qcd,"./rate_histos_qcd.root",""); 
-  // ScanChain(qcd,"./rate_histos_qcd_noSIP.root","noSIP"); 
+  ScanChain(qcd,"./rate_histos_qcd_noSIP.root","noSIP"); 
   // ScanChain(qcd,"./rate_histos_qcd_ptRel.root","ptRel"); 
   // ScanChain(qcd,"./rate_histos_qcd_ptRel_noSIP.root","ptRel_noSIP");
 
-  // ScanChain(qcd,"./rate_histos_qcd_doBonly.root","doBonly"); 
-  // ScanChain(qcd,"./rate_histos_qcd_noSIP_doBonly.root","noSIP_doBonly"); 
+  ScanChain(qcd,"./rate_histos_qcd_doBonly.root","doBonly"); 
+  ScanChain(qcd,"./rate_histos_qcd_noSIP_doBonly.root","noSIP_doBonly"); 
   // ScanChain(qcd,"./rate_histos_qcd_ptRel_doBonly.root","ptRel_doBonly"); 
   // ScanChain(qcd,"./rate_histos_qcd_ptRel_noSIP_doBonly.root","ptRel_noSIP_doBonly"); 
 
-  // ScanChain(qcd,"./rate_histos_qcd_doConly.root","doConly"); 
-  // ScanChain(qcd,"./rate_histos_qcd_noSIP_doConly.root","noSIP_doConly"); 
+  ScanChain(qcd,"./rate_histos_qcd_doConly.root","doConly"); 
+  ScanChain(qcd,"./rate_histos_qcd_noSIP_doConly.root","noSIP_doConly"); 
   // ScanChain(qcd,"./rate_histos_qcd_ptRel_doConly.root","ptRel_doBonly"); 
   // ScanChain(qcd,"./rate_histos_qcd_ptRel_noSIP_doConly.root","ptRel_noSIP_doBonly"); 
 
-  // ScanChain(qcd,"./rate_histos_qcd_doLightonly.root","doLightonly"); 
-  // ScanChain(qcd,"./rate_histos_qcd_noSIP_doLightonly.root","noSIP_doLightonly");
+  ScanChain(qcd,"./rate_histos_qcd_doLightonly.root","doLightonly"); 
+  ScanChain(qcd,"./rate_histos_qcd_noSIP_doLightonly.root","noSIP_doLightonly");
   // ScanChain(qcd,"./rate_histos_qcd_ptRel_doLightonly.root","ptRel_doBonly"); 
   // ScanChain(qcd,"./rate_histos_qcd_ptRel_noSIP_doLightonly.root","ptRel_noSIP_doBonly"); 
 
