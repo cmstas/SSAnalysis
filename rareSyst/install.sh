9b48a7325266993d68812514305a70e8cf3eedb6
Christopher A. George <george@physics.ucsb.edu>
2015-05-07 16:32:50 -0700

    add build file

--- rareSyst/install.sh
+++ rareSyst/install.sh
@@ -4,13 +4,13 @@
 export SCRAM_ARCH=slc6_amd64_gcc481
 
 #Install the Pythia --> Baby part
-cmsrel CMSSW_7_1_6
-cd CMSSW_7_1_6/src
+cmsrel CMSSW_7_2_0
+cd CMSSW_7_2_0/src
 eval `scramv1 runtime -sh`
-mkdir babyMaker
-cd babyMaker
-mkedprod babyMaker
-cd babyMaker
+mkdir producer
+cd producer
+mkedprod producer
+cd producer
 cp ../../../../ConfFile_cfg.py python/
 cp ../../../../CfiFile_cfi.py python/
 cp ../../../../filter_cfg.py python/
@@ -18,4 +18,5 @@ cp ../../../../MCUtilities.h plugins/
 cp ../../../../producer.cc plugins/
 cp ../../../../filter.cc plugins/
 cp ../../../../MCUtilities.cc plugins/
+cp ../../../../BuildFile.xml plugins/
 scram b -j 5
