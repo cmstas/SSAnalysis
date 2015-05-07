f8bcbca1d26050c73a05f159172fd66f82ec7462
Christopher A. George <george@physics.ucsb.edu>
2015-05-07 14:47:50 -0700

    update

--- yields/yields.C
+++ yields/yields.C
@@ -6,9 +6,6 @@
 //Tables on/off
 bool makeTables = 1;
 
-//Show only particular flavor (UNASSIGNED for all)
-hyp_type_t flavor = UNASSIGNED;
-
 //Lumi
 float lumi = 10.0;
 
@@ -21,7 +18,7 @@ float getYield(results_t results, int bin, int which){
   return -1;
 }
 
-results_t run(TChain* chain, string name){
+results_t run(TChain* chain, string name, hyp_type_t flavor = UNASSIGNED){
 
   //Declare hists
   TH1F *HighHighPlot = new TH1F(Form("%s_hh", name.c_str()), Form("%s_hh", name.c_str()), 32, 1, 33); 
@@ -107,7 +104,14 @@ void yields(){
   t5qqww_1200->Add("/nfs-7/userdata/ss2015/ssBabies/v1.08/private/t5qqqqWW_1200_1000_800_baby_multiIso.root"   );
   t5qqww_deg ->Add("/nfs-7/userdata/ss2015/ssBabies/v1.08/private/t5qqqqWW_deg_1000_315_300_baby_multiIso.root");
 
-  //Make the histograms
+  //Chains for type
+  TChain* all_bkgd = new TChain("t");
+  all_bkgd->Add(ttbar);
+  all_bkgd->Add(ttw);
+  all_bkgd->Add(ttz);
+  all_bkgd->Add(wz);
+
+  //Make the signal histograms
   results_t ttbar_graphs       = run(ttbar      , "ttbar"      );
   results_t ttw_graphs         = run(ttw        , "ttw"        );
   results_t ttz_graphs         = run(ttz        , "ttz"        );
@@ -117,10 +121,15 @@ void yields(){
   results_t t5qqww_1200_graphs = run(t5qqww_1200, "t5qqww_1200");
   results_t t5qqww_deg_graphs  = run(t5qqww_deg , "t5qqww_deg" );
 
+  //Make the type histograms
+  results_t bkgd_graphs_ee  = run(all_bkgd, "bkgd_ee", EE);
+  results_t bkgd_graphs_em  = run(all_bkgd, "bkgd_em", EM);
+  results_t bkgd_graphs_mm  = run(all_bkgd, "bkgd_mm", MM);
+
   //Prepare for plots -- null
   TH1F* null = new TH1F("","",1,0,1);
 
-  //Prepare for plots -- backgrounds
+  //Prepare for plots -- backgrounds, sample
   vector <TH1F*> background_high; 
   background_high.push_back(ttbar_graphs.hh);
   background_high.push_back(ttw_graphs.hh);
@@ -137,7 +146,21 @@ void yields(){
   background_low.push_back(ttz_graphs.ll);
   background_low.push_back(wz_graphs.ll);
 
-  //Prepare for plots -- signals
+  //Prepare for plots -- backgrounds, type
+  vector <TH1F*> background_type_high; 
+  background_type_high.push_back(bkgd_graphs_ee.hh);
+  background_type_high.push_back(bkgd_graphs_em.hh);
+  background_type_high.push_back(bkgd_graphs_mm.hh);
+  vector <TH1F*> background_type_hl; 
+  background_type_hl.push_back(bkgd_graphs_ee.hl);
+  background_type_hl.push_back(bkgd_graphs_em.hl);
+  background_type_hl.push_back(bkgd_graphs_mm.hl);
+  vector <TH1F*> background_type_low; 
+  background_type_low.push_back(bkgd_graphs_ee.ll);
+  background_type_low.push_back(bkgd_graphs_em.ll);
+  background_type_low.push_back(bkgd_graphs_mm.ll);
+
+  //Prepare for plots -- signals, sample
   vector <TH1F*> signal_high; 
   signal_high.push_back(t1tttt_1200_graphs.hh);
   signal_high.push_back(t1tttt_1500_graphs.hh);
@@ -154,23 +177,35 @@ void yields(){
   signal_low.push_back(t5qqww_1200_graphs.ll);
   signal_low.push_back(t5qqww_deg_graphs.ll);
 
-  //Prepare for plots -- signal titles
+  //Prepare for plots -- background titles
   vector <string> titles;
-  vector <string> signal_titles;
   titles.push_back("ttbar");
   titles.push_back("ttw");
   titles.push_back("ttz");
   titles.push_back("wz");
+
+  //Prepare for plots -- background titles (type)
+  vector <string> typetitles;
+  typetitles.push_back("ee");
+  typetitles.push_back("e#mu");
+  typetitles.push_back("#mu#mu");
+
+  //Prepare for plots -- signal titles
+  vector <string> signal_titles;
   signal_titles.push_back("t1tttt (1.2, 0.8)");
   signal_titles.push_back("t1tttt (1.5, 0.1)");
   signal_titles.push_back("t5qqww (1.2, 1.0, 0.8)");
   signal_titles.push_back("t5qqww (1.0, 0.315, 0.3)");
 
-  //Make plots
+  //Make plots -- sample
   dataMCplotMaker(null, background_high, titles, "H-H", "", "--vLine 9 --vLine 17 --vLine 25 --vLine 31 --outputName high_yields --noDivisionLabel --xAxisLabel SR --energy 13 --lumi 10 --nDivisions 210 --legendRight -0.00 --noXaxisUnit  --legendTextSize 0.0325 --isLinear"); 
   dataMCplotMaker(null, background_hl  , titles, "H-L", "multiIso", "--vLine 7 --vLine 13 --vLine 19 --vLine 23 --vLine 25 --outputName   hl_yields --noDivisionLabel --xAxisLabel SR --energy 13 --lumi 10 --nDivisions 210 --legendRight -0.00 --noXaxisUnit  --legendTextSize 0.0325 --isLinear"); 
   dataMCplotMaker(null, signal_high, signal_titles, "H-H", "", "--vLine 9 --vLine 17 --vLine 25 --vLine 31 --outputName high_yields_s --noDivisionLabel --xAxisLabel SR --energy 13 --lumi 10 --legendRight -0.12 --noXaxisUnit  --legendTextSize 0.0325 --noStack --nDivisions 210 --isLinear"); 
   dataMCplotMaker(null, signal_hl  , signal_titles, "H-L", "", "--vLine 7 --vLine 13 --vLine 19 --vLine 23 --vLine 25 --outputName   hl_yields_s --noDivisionLabel --xAxisLabel SR --energy 13 --lumi 10 --legendRight -0.12 --noXaxisUnit  --legendTextSize 0.03 --noStack  --nDivisions 210 --isLinear"); 
+
+  //Make plots -- type
+  dataMCplotMaker(null, background_type_high, typetitles, "H-H", "multiIso", "--vLine 9 --vLine 17 --vLine 25 --vLine 31 --outputName high_yields_t --noDivisionLabel --xAxisLabel SR --energy 13 --lumi 10 --nDivisions 210 --legendRight -0.00 --noXaxisUnit  --legendTextSize 0.0325 --isLinear"); 
+  dataMCplotMaker(null, background_type_hl  , typetitles, "H-L", "multiIso", "--vLine 7 --vLine 13 --vLine 19 --vLine 23 --vLine 25 --outputName hl_yields_t --noDivisionLabel --xAxisLabel SR --energy 13 --lumi 10 --nDivisions 210 --legendRight -0.00 --noXaxisUnit  --legendTextSize 0.0325 --isLinear"); 
 
   //Make tables
   if (!makeTables) return; 
