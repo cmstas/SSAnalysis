f14f4de5b8ded90379b2da90d7c6a3f329a312c1
Christopher A. George <george@physics.ucsb.edu>
2015-05-07 16:38:08 -0700

    update ConfFile

--- rareSyst/ConfFile_cfg.py
+++ rareSyst/ConfFile_cfg.py
@@ -8,18 +8,11 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        #'file:/hadoop/cms/store/user/cgeorge/SS_Syst_Study/qqWW/Pythia/pythiaEvents_qqww_dynam_mm_qcut60_2012way_all.root'
-        'file:/home/users/cgeorge/ss2015/rare_syst_study/CMSSW_7_1_6/src/runPythia/pythiaEvents_qqww_dynam_mm_2012way.root'
-        #'file:/hadoop/cms/store/user/cgeorge/SS_Syst_Study/qqWW/Pythia/pythiaEvents_qqww_fixed_mm_qcut65_10.root',
-        #'file:/hadoop/cms/store/user/cgeorge/SS_Syst_Study/qqWW/Pythia/pythiaEvents_qqww_fixed_mm_qcut65_9.root',
-        #'file:/hadoop/cms/store/user/cgeorge/SS_Syst_Study/qqWW/Pythia/pythiaEvents_qqww_fixed_mm_qcut65_8.root',
-        #'file:/hadoop/cms/store/user/cgeorge/SS_Syst_Study/qqWW/Pythia/pythiaEvents_qqww_fixed_mm_qcut65_7.root',
-        #'file:/hadoop/cms/store/user/cgeorge/SS_Syst_Study/qqWW/Pythia/pythiaEvents_qqww_fixed_mm_qcut65_6.root',
-        #'file:/hadoop/cms/store/user/cgeorge/SS_Syst_Study/qqWW/Pythia/pythiaEvents_qqww_fixed_mm_qcut65_5.root',
-        #'file:/hadoop/cms/store/user/cgeorge/SS_Syst_Study/qqWW/Pythia/pythiaEvents_qqww_fixed_mm_qcut65_4.root',
-        #'file:/hadoop/cms/store/user/cgeorge/SS_Syst_Study/qqWW/Pythia/pythiaEvents_qqww_fixed_mm_qcut65_3.root',
-        #'file:/hadoop/cms/store/user/cgeorge/SS_Syst_Study/qqWW/Pythia/pythiaEvents_qqww_fixed_mm_qcut65_2.root',
-        #'file:/hadoop/cms/store/user/cgeorge/SS_Syst_Study/qqWW/Pythia/pythiaEvents_qqww_fixed_mm_qcut65_1.root'
+        'file:///hadoop/cms/store/user/cgeorge/SS_Syst_Study/ttW/Pythia/pythiaEvents_1M_fixed_1.root',
+        'file:///hadoop/cms/store/user/cgeorge/SS_Syst_Study/ttW/Pythia/pythiaEvents_1M_fixed_2.root',
+        'file:///hadoop/cms/store/user/cgeorge/SS_Syst_Study/ttW/Pythia/pythiaEvents_1M_fixed_3.root',
+        'file:///hadoop/cms/store/user/cgeorge/SS_Syst_Study/ttW/Pythia/pythiaEvents_1M_fixed_4.root',
+        'file:///hadoop/cms/store/user/cgeorge/SS_Syst_Study/ttW/Pythia/pythiaEvents_1M_fixed_5.root'
     ),
     duplicateCheckMode = cms.untracked.string('checkEachFile')
 )
@@ -40,12 +33,11 @@ process.AGfilterEventSelect = cms.PSet(
 
 process.out = cms.OutputModule("PoolOutputModule",
     process.AGfilterEventSelect,
-    fileName = cms.untracked.string('qqWW_baby_dynam_mm_qcut60_2012way_noFilt.root'),
+    fileName = cms.untracked.string('TTWbaby_1M_fixed_noFilt.root'),
     outputCommands = cms.untracked.vstring('drop *', 
       'keep *_*_*_OWNPARTICLES'
     )
 )
-
   
 process.p = cms.Path(process.producer)
 #process.p = cms.Path(process.producer*process.AGfilter)
