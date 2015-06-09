574d5520882d29d9a1bf46369b7b39dab433e000
Christopher A. George <george@physics.ucsb.edu>
2015-06-09 01:01:02 -0700

    revert for Giuseppe

--- FakeRate/app_region/doAll.C
+++ FakeRate/app_region/doAll.C
@@ -1,13 +1,20 @@
 {
-  gSystem->Load("../../CORE/CMS3_CORE.so");
-  gSystem->Load("../../Tools/libTools.so");
+  //not sure which ones I need
+  gROOT->ProcessLine(".L ../../CORE/CMS3.cc+");
+  gROOT->ProcessLine(".L ../../CORE/Base.cc+");
+  gROOT->ProcessLine(".L ../../CORE/IsolationTools.cc+");
+  gROOT->ProcessLine(".L ../../CORE/ElectronSelections.cc+");
+  gROOT->ProcessLine(".L ../../CORE/MuonSelections.cc+");
+  gROOT->ProcessLine(".L ../../CORE/MCSelections.cc+");
+  gROOT->ProcessLine(".L ../../CORE/JetSelections.cc+");
+  gROOT->ProcessLine(".L ../../CORE/SSSelections.cc+");
+  gROOT->ProcessLine(".L ../../Tools/utils.cc+");
 
-  gROOT->ProcessLine(".L lepfilter.cc+");
   gROOT->ProcessLine(".L ScanChain.C+");
 
   bool doConeCorr   = 1;
   bool doJetCorr    = 0;
-  bool doLooseEMVA  = 0;
+  bool doLooseEMVA  = 1;
   bool doPtRatioCor = 0;
   bool doBonly      = 0;
   bool doConly      = 0;
@@ -30,15 +37,17 @@
   else if (lowlow)   ptRegion = "LL";
 
   TString fakeratefile = "../measurement_region/rate_histos_qcd"+option+".root";
+  // TString fakeratefile = "../measurement_region/rate_histos_qcdnonEnriched"+option+".root";
+  // TString fakeratefile = "../measurement_region/rate_histos_ttbar.root"; //from applying FR to ttbar FR baby
 
-  if (doConeCorr) option+="_coneCorr";     //option only for ScanChain
-  else if (doJetCorr) option+="_jetCorr";  //option only for ScanChain
+  if (doConeCorr) option+="_coneCorr";//option only for ScanChain
+  else if (doJetCorr) option+="_jetCorr";//option only for ScanChain
 
   TChain *ch = new TChain("t"); 
-  if (doLooseEMVA) ch->Add("../../babies/v1.16_looseEMVA/TTBAR.root");
-  else ch->Add("../../babies/v1.21/TTBAR.root");
+  if (doLooseEMVA) ch->Add("../../babies/v1.16_looseEMVA/TTBAR_multiIso.root");
+  else ch->Add("../../babies/v1.16/TTBAR_multiIso.root");
   ScanChain(ch, fakeratefile, option, ptRegion); 
 
-  //TChain *ch_wjets = new TChain("t"); 
-  //ch_wjets->Add("/nfs-7/userdata/ss2015/ssBabies/v1.04/Wjets_baby.root"); //this one!
+  TChain *ch_wjets = new TChain("t"); 
+  ch_wjets->Add("/nfs-7/userdata/ss2015/ssBabies/v1.04/Wjets_baby.root"); //this one!
 }
