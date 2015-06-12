aae5b932bd173910a2ca87306707f46195c1a7de
Giuseppe Cerati <giuseppe.cerati@cern.ch>
2015-06-12 12:06:47 +0200

    load precompiled libraries

--- cards/doAll.C
+++ cards/doAll.C
@@ -2,15 +2,8 @@
 #include "TSystem.h"
 void doAll(){
   gROOT->ProcessLine(".L ../software/dataMCplotMaker/dataMCplotMaker.cc+");
-  gROOT->ProcessLine(".L ../software/tableMaker/CTable.cpp+");
-  gROOT->ProcessLine(".L ../CORE/CMS3.cc+");
-  gROOT->ProcessLine(".L ../CORE/Base.cc+");
-  gROOT->ProcessLine(".L ../CORE/IsolationTools.cc+");
-  gROOT->ProcessLine(".L ../CORE/ElectronSelections.cc+");
-  gROOT->ProcessLine(".L ../CORE/MuonSelections.cc+");
-  gROOT->ProcessLine(".L ../CORE/MCSelections.cc+");
-  gROOT->ProcessLine(".L ../CORE/JetSelections.cc+");
-  gROOT->ProcessLine(".L ../CORE/SSSelections.cc+");
+  gSystem->Load("../software/tableMaker/libSimpleTable.so");
+  gSystem->Load("../CORE/CMS3_CORE.so");
   gROOT->ProcessLine(".L SS.cc++");
   gROOT->ProcessLine(".L yields.C++");
   gROOT->ProcessLine("yields()");
