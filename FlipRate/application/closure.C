7aa9685b81993e727df759d34556f3adb3a84a2b
Christopher A. George <george@physics.ucsb.edu>
2015-07-01 21:10:21 -0700

    add errors

--- FlipRate/application/closure.C
+++ FlipRate/application/closure.C
@@ -35,13 +35,23 @@ void closure(){
   TH2D  *rate = (TH2D*)flip_file->Get("flips"); 
 
   //Closure vs. inv mass plot
-  //TH1F* clos_MC   = new TH1F("clos_plot_MC"  , "clos_plot_MC"  , 9, 70, 115); 
-  //TH1F* clos_data = new TH1F("clos_plot_data", "clos_plot_data", 9, 70, 115); 
-  TH1F* clos_MC   = new TH1F("clos_plot_MC"  , "clos_plot_MC"  , 17, 30, 115); 
-  TH1F* clos_data = new TH1F("clos_plot_data", "clos_plot_data", 17, 30, 115); 
+  TH1F* clos_MC   = new TH1F("clos_plot_MC"  , "clos_plot_MC"  , 9, 70, 115); 
+  TH1F* clos_data = new TH1F("clos_plot_data", "clos_plot_data", 9, 70, 115); 
 
+  //Errors -- keep track of number of events in each FR bin so we can get the error 
+  int nBinsX = 5; 
+  int nBinsY = 3; 
+  float xbins[] = { 0, 20, 40, 60, 80, 100 }; 
+  float ybins[] = { 0, 1, 2, 3 }; 
+  TH2D* errors  = new TH2D("errors" , "errors" , nBinsX, xbins, nBinsY, ybins);
+
+  //Results
   float nObs  = 0;
   float nPred = 0;
+
+  //Pred err variables
+  float stat2 = 0; 
+  float fr_err2 = 0; 
 
   //Set up chains
   TChain *chain = new TChain("t");
@@ -80,6 +90,9 @@ void closure(){
       //Throw away unneeded events
       if (hyp_class() != 3 && hyp_class() != 4) continue;
 
+      //Keep only electron-electron events
+      if (abs(lep1_id()) != 11 || abs(lep2_id()) != 11) continue;
+
       //Weight
       float weight = scale1fb()*theLumi;
 
@@ -94,13 +107,28 @@ void closure(){
         float ff = getFlipFactor(rate);
         nPred += ff*weight;
         clos_MC->Fill( (lep1_p4() + lep2_p4()).M(), ff*weight); 
+        errors->Fill(lep1_p4().pt(), fabs(lep1_p4().eta()), weight);
+        errors->Fill(lep2_p4().pt(), fabs(lep2_p4().eta()), weight);
+        stat2 += pow(ff*weight, 2); 
       }
 
     }//event loop
   }//file loop
 
-  cout << "pred: " << nPred << endl;
-  cout << " obs: " << nObs << endl;
+  //Figure out error from "errors" plot:
+  for (int i = 1; i <= errors->GetNbinsX()+1; i++){
+    for (int j = 1; j <= errors->GetNbinsY(); j++){
+      float FR_val = rate->GetBinContent(i, j);
+      float FR_err = rate->GetBinError(i, j);
+      float nEntries = errors->GetBinContent(i, j); 
+      float error = FR_err*nEntries/pow(1-FR_val,2);
+      fr_err2 += pow(error, 2);
+    }
+  }
+
+  cout << "pred: " << nPred << " pm " << sqrt(stat2 + fr_err2) << endl;
+  cout << " obs: " << nObs << " pm " << sqrt(nObs) << endl;
+
 
   //Make plot
   vector <TH1F*> bkgd;
@@ -108,8 +136,5 @@ void closure(){
   vector <string> titles;
   titles.push_back("pred"); 
   dataMCplotMaker(clos_data, bkgd, titles, "flip closure", "", "--outputName flip_closure.pdf --xAxisLabel M_{ll} --lumi 1 --dataName obs --topYaxisTitle obs/pred --isLinear --noFill"); 
-  cout << bkgd[0]->Integral() << endl;
-  cout << clos_data->Integral() << endl;
-  for (unsigned int i = 0; i < clos_data->GetNbinsX()+1; i++) cout << i << " " << clos_data->GetBinContent(i) << endl;
 
 }
