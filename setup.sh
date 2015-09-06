bf0652b94642d6f4f0eb41955fef653f0ecccd08
Nick Amin <amin.nj@gmail.com>
2015-09-05 17:08:52 -0700

    make setup.sh nicer

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD:$PWD/batch
 
 git update-index --assume-unchanged main.cc
 
-cd CMSSW_7_4_1_patch1/
+therelease=CMSSW_7_4_1_patch1
+export SCRAM_ARCH=slc6_amd64_gcc491
+if [ ! -d $therelease ]; then cmsrel $therelease; fi
+cd $therelease
 eval `scram ru -sh`
 cd -
