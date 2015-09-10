a8f2ed84d5db8c51bf935b45fca5460e2b5d41d1
Christopher A. George <george@physics.ucsb.edu>
2015-09-09 22:52:33 -0700

    add more samples

--- batch/base.h
+++ batch/base.h
@@ -1,6 +1,6 @@
 #ifndef THESSBATCHBASE_H
 #define THESSBATCHBASE_H
 
-enum sample_t { TTBAR = 0, TTW = 1, TTZL = 2, WZ = 3, T1TTTT_1500 = 4, T1TTTT_1200 = 5, DY_low = 6, DY_high = 7, WJets = 8, DataDoubleMuon = 9, DataDoubleEG = 10, TTPOWHEG = 11, TTZQ = 18, DATAMUONEG = 16, SINGLETOP1 = 12, SINGLETOP2 = 13, SINGLETOP3 = 14, SINGLETOP4 = 15, SINGLETOP5 = 17, DATAMUONEGC = 19, DataDoubleMuonC = 20, DataDoubleEGC = 21, WZ3LNu = 22, TTWQQ = 23, DATAMUONEG2 = 24, DataDoubleMuon2 = 25, DataDoubleEG2 = 26 };
+enum sample_t { TTBAR = 0, TTW = 1, TTZL = 2, WZ = 3, T1TTTT_1500 = 4, T1TTTT_1200 = 5, DY_low = 6, DY_high = 7, WJets = 8, DataDoubleMuon = 9, DataDoubleEG = 10, TTPOWHEG = 11, TTZQ = 18, DATAMUONEG = 16, SINGLETOP1 = 12, SINGLETOP2 = 13, SINGLETOP3 = 14, SINGLETOP4 = 15, SINGLETOP5 = 17, DATAMUONEGC = 19, DataDoubleMuonC = 20, DataDoubleEGC = 21, WZ3LNu = 22, TTWQQ = 23, DATAMUONEG2 = 24, DataDoubleMuon2 = 25, DataDoubleEG2 = 26, WGToLNuG = 27, TTG = 28, TTHtoNonBB = 29, VHtoNonBB = 30, TZQ = 31, TTTT = 32, WWDPS = 33, WZZ = 34 };
 
 #endif
