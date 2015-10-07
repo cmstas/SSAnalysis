33ac543ac45c7b9200d53d065647769028989308
Giuseppe <giuseppe.cerati@cern.ch>
2015-10-07 13:27:42 -0700

    update yieldsAndSRs

--- yieldsAndSRs/do.C
+++ yieldsAndSRs/do.C
@@ -1,6 +1,8 @@
 {
+
   gROOT->ProcessLine(".L ../software/dataMCplotMaker/dataMCplotMaker.cc+");
   //gSystem->Load("../software/tableMaker/libSimpleTable.so");
+  gROOT->ProcessLine(".L ../CORE/Tools/dorky/dorky.cc++");
   gSystem->Load("../CORE/CMS3_CORE.so");
   gROOT->ProcessLine(".L SS.cc+");
   gROOT->ProcessLine(".L yields.C++");
