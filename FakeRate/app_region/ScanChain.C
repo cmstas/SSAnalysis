d85af05fefdc7f681dfab2004073809f3c1be758
Nick Amin <amin.nj@gmail.com>
2015-10-11 21:55:18 -0700

    fix issue with data in app region

--- FakeRate/app_region/ScanChain.C
+++ FakeRate/app_region/ScanChain.C
@@ -241,7 +241,7 @@ int ScanChain( TChain* chain, TString fakeratefile, TString option = "", TString
   bool highlow = ptRegion.Contains("HL") ? true : false;
   bool lowlow = ptRegion.Contains("LL") ? true : false;
 
-  float luminosity = 16.1/1000; // ifb
+  float luminosity = 225.6/1000.0; // ifb
 
   //Dir
   TDirectory *rootdir = gDirectory->GetDirectory("Rint:");
@@ -487,7 +487,7 @@ int ScanChain( TChain* chain, TString fakeratefile, TString option = "", TString
       }
 
       // Analysis Code
-      float weight = ss::is_real_data() ? 1 : ss::scale1fb()*luminosity;
+      float weight = ss::is_real_data() ? 1.0 : ss::scale1fb()*luminosity;
 
       // ignore MC part of chain when looking at data, except for contamination subtraction
       if(doData && !ss::is_real_data()) weight = 0; 
@@ -626,79 +626,82 @@ int ScanChain( TChain* chain, TString fakeratefile, TString option = "", TString
         //Counters
         counter += (doData && ss::is_real_data()) || (!doData);
         Nss_reco = Nss_reco + weight;
-        if( isLep1Prompt && isLep2Prompt ){
-          prompt2_reco = prompt2_reco + weight;
-          NpromptL1_reco = NpromptL1_reco + weight;
-          NpromptL2_reco = NpromptL2_reco + weight;
-        }
-        else if ( ss::lep1_motherID()==2 || ss::lep2_motherID()==2 ) sign_misid_reco += weight; 
+        // if( isLep1Prompt && isLep2Prompt ){
+        //   prompt2_reco = prompt2_reco + weight;
+        //   NpromptL1_reco = NpromptL1_reco + weight;
+        //   NpromptL2_reco = NpromptL2_reco + weight;
+        // }
+        // else if ( ss::lep1_motherID()==2 || ss::lep2_motherID()==2 ) sign_misid_reco += weight; 
 
-        //1) Lep 2 is non-prompt
-        else if( isLep1Prompt && isLep2NonPrompt ){ 
-          prompt1_reco += weight;  
-          NpromptL1_reco += weight;
-          hists[getHist("Npn_histo_sr_obs")   ]->Fill(sr, weight);
-          hists[getHist("Npn_histo_br_obs")   ]->Fill(br, weight);
-          hists[getHist("Npn_histo_HT_obs")   ]->Fill(ss::ht(), weight);
-          hists[getHist("Npn_histo_MET_obs")  ]->Fill(ss::met(), weight);
-          hists[getHist("Npn_histo_MTMIN_obs")]->Fill(mtmin, weight);
-          hists[getHist("Npn_histo_L1PT_obs") ]->Fill(coneCorr ? lep1_pT : ss::lep1_p4().pt(), weight);
-          hists[getHist("Npn_histo_L2PT_obs") ]->Fill(coneCorr ? lep2_pT : ss::lep2_p4().pt(), weight);
+
+        // //1) Lep 2 is non-prompt
+        // else if( isLep1Prompt && isLep2NonPrompt ){ 
+          prompt2_reco += weight;  
+
+          e1 = getFakeRate(rate_histo_e, lep1_pT, fabs(ss::lep1_p4().eta()), ss::ht(), false );
+          float mult = (e1/(1-e1));
+          hists[getHist("Npn_histo_sr_obs")   ]->Fill(sr, mult*weight);
+          hists[getHist("Npn_histo_br_obs")   ]->Fill(br, mult*weight);
+          hists[getHist("Npn_histo_HT_obs")   ]->Fill(ss::ht(), mult*weight);
+          hists[getHist("Npn_histo_MET_obs")  ]->Fill(ss::met(), mult*weight);
+          hists[getHist("Npn_histo_MTMIN_obs")]->Fill(mtmin, mult*weight);
+          hists[getHist("Npn_histo_L1PT_obs") ]->Fill(coneCorr ? lep1_pT : ss::lep1_p4().pt(), mult*weight);
+          hists[getHist("Npn_histo_L2PT_obs") ]->Fill(coneCorr ? lep2_pT : ss::lep2_p4().pt(), mult*weight);
 
           if(abs(ss::lep2_id()) == 11){
-            hists[getHist("Npn_histo_sr_obs_el")   ]->Fill(sr, weight);
-            hists[getHist("Npn_histo_br_obs_el")   ]->Fill(br, weight);
-            hists[getHist("Npn_histo_HT_obs_el")   ]->Fill(ss::ht(), weight);
-            hists[getHist("Npn_histo_MET_obs_el")  ]->Fill(ss::met(), weight);
-            hists[getHist("Npn_histo_MTMIN_obs_el")]->Fill(mtmin, weight);
-            hists[getHist("Npn_histo_L1PT_obs_el") ]->Fill(coneCorr ? lep1_pT : ss::lep1_p4().pt(), weight);
-            hists[getHist("Npn_histo_L2PT_obs_el") ]->Fill(coneCorr ? lep2_pT : ss::lep2_p4().pt(), weight);
+            hists[getHist("Npn_histo_sr_obs_el")   ]->Fill(sr, mult*weight);
+            hists[getHist("Npn_histo_br_obs_el")   ]->Fill(br, mult*weight);
+            hists[getHist("Npn_histo_HT_obs_el")   ]->Fill(ss::ht(), mult*weight);
+            hists[getHist("Npn_histo_MET_obs_el")  ]->Fill(ss::met(), mult*weight);
+            hists[getHist("Npn_histo_MTMIN_obs_el")]->Fill(mtmin, mult*weight);
+            hists[getHist("Npn_histo_L1PT_obs_el") ]->Fill(coneCorr ? lep1_pT : ss::lep1_p4().pt(), mult*weight);
+            hists[getHist("Npn_histo_L2PT_obs_el") ]->Fill(coneCorr ? lep2_pT : ss::lep2_p4().pt(), mult*weight);
           } 
           else if(abs(ss::lep2_id()) == 13){
-            hists[getHist("Npn_histo_sr_obs_mu")   ]->Fill(sr, weight);
-            hists[getHist("Npn_histo_br_obs_mu")   ]->Fill(br, weight);
-            hists[getHist("Npn_histo_HT_obs_mu")   ]->Fill(ss::ht(), weight);
-            hists[getHist("Npn_histo_MET_obs_mu")  ]->Fill(ss::met(), weight);
-            hists[getHist("Npn_histo_MTMIN_obs_mu")]->Fill(mtmin, weight);
-            hists[getHist("Npn_histo_L1PT_obs_mu") ]->Fill(coneCorr ? lep1_pT : ss::lep1_p4().pt(), weight);
-            hists[getHist("Npn_histo_L2PT_obs_mu") ]->Fill(coneCorr ? lep2_pT : ss::lep2_p4().pt(), weight);
+            hists[getHist("Npn_histo_sr_obs_mu")   ]->Fill(sr, mult*weight);
+            hists[getHist("Npn_histo_br_obs_mu")   ]->Fill(br, mult*weight);
+            hists[getHist("Npn_histo_HT_obs_mu")   ]->Fill(ss::ht(), mult*weight);
+            hists[getHist("Npn_histo_MET_obs_mu")  ]->Fill(ss::met(), mult*weight);
+            hists[getHist("Npn_histo_MTMIN_obs_mu")]->Fill(mtmin, mult*weight);
+            hists[getHist("Npn_histo_L1PT_obs_mu") ]->Fill(coneCorr ? lep1_pT : ss::lep1_p4().pt(), mult*weight);
+            hists[getHist("Npn_histo_L2PT_obs_mu") ]->Fill(coneCorr ? lep2_pT : ss::lep2_p4().pt(), mult*weight);
           }
-        }
+        // }
 
-        //2) Lep 1 is non-prompt
-        else if( isLep1NonPrompt && isLep2Prompt ){ 
-          prompt1_reco = prompt1_reco + weight; 
-          NpromptL2_reco = NpromptL2_reco + weight;				
-          hists[getHist("Npn_histo_sr_obs")]   ->Fill(sr, weight);
-          hists[getHist("Npn_histo_br_obs")]   ->Fill(br, weight);
-          hists[getHist("Npn_histo_HT_obs")]   ->Fill(ss::ht(), weight);
-          hists[getHist("Npn_histo_MET_obs")]  ->Fill(ss::met(), weight);
-          hists[getHist("Npn_histo_MTMIN_obs")]->Fill(mtmin, weight);
-          hists[getHist("Npn_histo_L1PT_obs") ]->Fill(coneCorr ? lep1_pT : ss::lep1_p4().pt(), weight);
-          hists[getHist("Npn_histo_L2PT_obs") ]->Fill(coneCorr ? lep2_pT : ss::lep2_p4().pt(), weight);
+        // //2) Lep 1 is non-prompt
+        // else if( isLep1NonPrompt && isLep2Prompt ){ 
+        //   prompt1_reco = prompt1_reco + weight; 
+        //   NpromptL2_reco = NpromptL2_reco + weight;				
+        //   hists[getHist("Npn_histo_sr_obs")]   ->Fill(sr, weight);
+        //   hists[getHist("Npn_histo_br_obs")]   ->Fill(br, weight);
+        //   hists[getHist("Npn_histo_HT_obs")]   ->Fill(ss::ht(), weight);
+        //   hists[getHist("Npn_histo_MET_obs")]  ->Fill(ss::met(), weight);
+        //   hists[getHist("Npn_histo_MTMIN_obs")]->Fill(mtmin, weight);
+        //   hists[getHist("Npn_histo_L1PT_obs") ]->Fill(coneCorr ? lep1_pT : ss::lep1_p4().pt(), weight);
+        //   hists[getHist("Npn_histo_L2PT_obs") ]->Fill(coneCorr ? lep2_pT : ss::lep2_p4().pt(), weight);
 
-          if(abs(ss::lep1_id()) == 11){
-            hists[getHist("Npn_histo_sr_obs_el")]   ->Fill(sr, weight);
-            hists[getHist("Npn_histo_br_obs_el")]   ->Fill(br, weight);
-            hists[getHist("Npn_histo_HT_obs_el")]   ->Fill(ss::ht(), weight);
-            hists[getHist("Npn_histo_MET_obs_el")]  ->Fill(ss::met(), weight);
-            hists[getHist("Npn_histo_MTMIN_obs_el")]->Fill(mtmin, weight);
-            hists[getHist("Npn_histo_L1PT_obs_el")] ->Fill(coneCorr ? lep1_pT : ss::lep1_p4().pt(), weight);
-            hists[getHist("Npn_histo_L2PT_obs_el")] ->Fill(coneCorr ? lep2_pT : ss::lep2_p4().pt(), weight);
-          } 
-          else if(abs(ss::lep1_id()) == 13){
-            hists[getHist("Npn_histo_sr_obs_mu")]   ->Fill(sr, weight);
-            hists[getHist("Npn_histo_br_obs_mu")]   ->Fill(br, weight);
-            hists[getHist("Npn_histo_HT_obs_mu")]   ->Fill(ss::ht(), weight);
-            hists[getHist("Npn_histo_MET_obs_mu")]  ->Fill(ss::met(), weight);
-            hists[getHist("Npn_histo_MTMIN_obs_mu")]->Fill(mtmin, weight);
-            hists[getHist("Npn_histo_L1PT_obs_mu")] ->Fill(coneCorr ? lep1_pT : ss::lep1_p4().pt(), weight);
-            hists[getHist("Npn_histo_L2PT_obs_mu")] ->Fill(coneCorr ? lep2_pT : ss::lep2_p4().pt(), weight);
-          }
-        }
+        //   if(abs(ss::lep1_id()) == 11){
+        //     hists[getHist("Npn_histo_sr_obs_el")]   ->Fill(sr, weight);
+        //     hists[getHist("Npn_histo_br_obs_el")]   ->Fill(br, weight);
+        //     hists[getHist("Npn_histo_HT_obs_el")]   ->Fill(ss::ht(), weight);
+        //     hists[getHist("Npn_histo_MET_obs_el")]  ->Fill(ss::met(), weight);
+        //     hists[getHist("Npn_histo_MTMIN_obs_el")]->Fill(mtmin, weight);
+        //     hists[getHist("Npn_histo_L1PT_obs_el")] ->Fill(coneCorr ? lep1_pT : ss::lep1_p4().pt(), weight);
+        //     hists[getHist("Npn_histo_L2PT_obs_el")] ->Fill(coneCorr ? lep2_pT : ss::lep2_p4().pt(), weight);
+        //   } 
+        //   else if(abs(ss::lep1_id()) == 13){
+        //     hists[getHist("Npn_histo_sr_obs_mu")]   ->Fill(sr, weight);
+        //     hists[getHist("Npn_histo_br_obs_mu")]   ->Fill(br, weight);
+        //     hists[getHist("Npn_histo_HT_obs_mu")]   ->Fill(ss::ht(), weight);
+        //     hists[getHist("Npn_histo_MET_obs_mu")]  ->Fill(ss::met(), weight);
+        //     hists[getHist("Npn_histo_MTMIN_obs_mu")]->Fill(mtmin, weight);
+        //     hists[getHist("Npn_histo_L1PT_obs_mu")] ->Fill(coneCorr ? lep1_pT : ss::lep1_p4().pt(), weight);
+        //     hists[getHist("Npn_histo_L2PT_obs_mu")] ->Fill(coneCorr ? lep2_pT : ss::lep2_p4().pt(), weight);
+        //   }
+        // }
 
         //Both are non-prompt
-        else if( isLep1NonPrompt && isLep2NonPrompt ) prompt0_reco += weight;
+        // else if( isLep1NonPrompt && isLep2NonPrompt ) prompt0_reco += weight;
 
         //check for charge misID on gen level.
         if (ss::lep1_motherID()==2 || ss::lep2_motherID()==2) sign_misid_gen += weight;
@@ -745,6 +748,7 @@ int ScanChain( TChain* chain, TString fakeratefile, TString option = "", TString
         //1) Lep1 is tight, lep2 is loose!tight
         if (lep1_passes_id && !lep2_passes_id){  
 
+
           //Baseline selections
           if (!inSitu && ss::hyp_class() != 2) continue;
           if (inSitu && (ss::lep2_multiIso() || !isFakeLeg(2) || !isGoodLeg(1) || !lep2_denom_iso)) continue;
@@ -759,7 +763,9 @@ int ScanChain( TChain* chain, TString fakeratefile, TString option = "", TString
           //if (abs(ss::lep2_id()) == 11 && fabs(ss::lep2_p4().eta()) > 1.479 && ss::lep2_MVA() < 0.05) continue; 
         
           if(doData && !ss::is_real_data() && isGoodLeg(2)) weight = -ss::scale1fb(); 
-          else weight = 0;
+          else {
+              if(!ss::is_real_data()) weight = 0;
+          }
  
           if (usePtRatioCor){
             //this is a tighter FO than default, so skip if it does not pass
