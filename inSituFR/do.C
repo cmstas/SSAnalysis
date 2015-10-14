48a3baa8b252172af4f375749e2e32fbb8bb4bab
Christopher A. George <george@physics.ucsb.edu>
2015-10-14 04:45:36 -0700

    update derivation of fakes code

--- inSituFR/do.C
+++ inSituFR/do.C
@@ -1,8 +1,6 @@
 {
-  gROOT->ProcessLine(".L ../software/dataMCplotMaker/dataMCplotMaker.cc++");
-  gROOT->ProcessLine(".L ../software/dataMCplotMaker/PlotMaker2D.cc++");
-  gSystem->Load("../CORE/CMS3_CORE.so");
-  gROOT->ProcessLine(".L SS.cc++");
-  gROOT->ProcessLine(".L FR.C++");
+  gROOT->ProcessLine(".L ../software/dataMCplotMaker/PlotMaker2D.cc+");
+  gROOT->ProcessLine(".L SS.cc+");
+  gROOT->ProcessLine(".L FR.C+");
   gROOT->ProcessLine("FR()");
 }
