b0247f65de974f6d87773c260adaad92efde7c81
Christopher A. George <george@physics.ucsb.edu>
2015-10-24 10:17:09 -0700

    updates

--- sync/do.C
+++ sync/do.C
@@ -2,7 +2,8 @@
   gROOT->ProcessLine(".L ../software/dataMCplotMaker/dataMCplotMaker.cc++");
   gSystem->Load("../software/tableMaker/libSimpleTable.so");
   gSystem->Load("../CORE/CMS3_CORE.so");
-  gROOT->ProcessLine(".L SS.cc+");
+  gROOT->ProcessLine(".L ../classFiles/v4.00/SS.cc+");
+  gROOT->ProcessLine(".L ../CORE/Tools/dorky/dorky.cc+");
   gROOT->ProcessLine(".L yields.C+");
   gROOT->ProcessLine("yields()");
 }
