fda5049570745e99fc0e61e18b29674279a065b3
Christopher A. George <george@physics.ucsb.edu>
2015-11-12 23:08:34 -0800

    again

--- batch/submit25ns.sh
+++ batch/submit25ns.sh
@@ -37,7 +37,7 @@ sed -i "s/cgeorge/$USER/" condorExecutable.sh
 
 #Then submit jobs
 nIter=0
-for sname in "TTW" "WJets_LO"  "ZG" "T1TTTT_1200" "T1TTTT_1500" "TG"  "T5QQQQWW_1200_1000_800" "T5QQQQWWDeg_1000_315_300" "T5ttttDeg_1000_300_285_280" "T6TTWW_600_425_50" "T6TTWW_650_150_50"  "SINGLETOP1" "SINGLETOP2" "SINGLETOP3" "SINGLETOP4" "SINGLETOP5" "TTWQQ" "TTZQ" "WWZ" "ZZ" "QQWW" "TTBAR"  "DataDoubleMuonD_05oct" "DataDoubleEGD_05oct" "DATAMUONEGD_05oct" "DataDoubleMuonD_v4" "DataDoubleEGD_v4" "DATAMUONEGD_v4" "WJets" "TTZL" "DY_high" "DY_low" "WGToLNuG" "TTG" "TTHtoNonBB" "VHtoNonBB"  "TZQ"  "TTTT"  "WWDPS" "WZZ" "TTBAR_PH"  "DataDoubleMuonC_05oct" "DataDoubleEGC_05oct" "DATAMUONEGC_05oct"
+for sname in "TTBAR" "TTW" "WJets_LO"  "ZG" "T1TTTT_1200" "T1TTTT_1500" "TG"  "T5QQQQWW_1200_1000_800" "T5QQQQWWDeg_1000_315_300" "T5ttttDeg_1000_300_285_280" "T6TTWW_600_425_50" "T6TTWW_650_150_50"  "SINGLETOP1" "SINGLETOP2" "SINGLETOP3" "SINGLETOP4" "SINGLETOP5" "TTWQQ" "TTZQ" "WWZ" "ZZ" "QQWW" "TTBAR"  "DataDoubleMuonD_05oct" "DataDoubleEGD_05oct" "DATAMUONEGD_05oct" "DataDoubleMuonD_v4" "DataDoubleEGD_v4" "DATAMUONEGD_v4" "WJets" "TTZL" "DY_high" "DY_low" "WGToLNuG" "TTG" "TTHtoNonBB" "VHtoNonBB"  "TZQ"  "TTTT"  "WWDPS" "WZZ" "TTBAR_PH"  "DataDoubleMuonC_05oct" "DataDoubleEGC_05oct" "DATAMUONEGC_05oct"
 do
   path="/hadoop/cms/store/group/snt/run2_25ns_MiniAODv2"
   #Iter
@@ -293,16 +293,16 @@ do
     number=$(( $i + 1 ))
 
     #Except they've finished
-    if [ -e /hadoop/cms/store/user/$USER/condor/ss_13_babies/$SPPATH/${sname_lower}_${number}_$expt.root ] 
+    if [ -e /hadoop/cms/store/user/$USER/condor/ss_13_babies/$SPPATH/${sname_lower}_${number}.root ] 
     then 
       continue
     fi
 
     echo "-------------"
-    echo "Working on $sname $number $expt"
+    echo "Working on $sname $number"
   
     #Or if they're still running
-    if [ -e /data/tmp/cgeorge/logs/condorLog_${nameNu}_${number}_$expt.log ] 
+    if [ -e /data/tmp/cgeorge/logs/condorLog_${nameNu}_${number}.log ] 
     then
       while read line
       do
@@ -313,7 +313,7 @@ do
         then
           process=`echo $line | awk '{ print $3 }'`
         fi
-      done < /data/tmp/cgeorge/logs/condorLog_${nameNu}_${number}_$expt.log
+      done < /data/tmp/cgeorge/logs/condorLog_${nameNu}_${number}.log
       jobid="$cluster.$process"
       condor_q $jobid > temp.txt
       result=`more temp.txt | awk 'END{print $1}'`
@@ -338,7 +338,6 @@ do
     sed -i s/ARG1/$nameNu/g condorFile
     sed -i s/ARG2/$number/g condorFile
     sed -i s/ARG3/$USER/g condorFile
-    sed -i s/ARG4/$expt/g condorFile
     sed -i "s,USER_PROXY,$pathToProxy,g" condorFile
     condor_submit condorFile
   done
