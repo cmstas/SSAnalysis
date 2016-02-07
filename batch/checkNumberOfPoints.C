cd9fccac7707bbce1b3d1489b54a23ef9fa0eae7
Christopher A. George <george@physics.ucsb.edu>
2016-02-07 00:14:06 -0800

    updates

--- batch/checkNumberOfPoints.C
+++ batch/checkNumberOfPoints.C
@@ -4,7 +4,7 @@
   //TFile *file = new TFile("/nfs-7/userdata/ss2015/ssBabies/v6.02-fs/T5QQQQVV_1100to1175_1to1050.root");
   //TFile *file = new TFile("/nfs-7/userdata/ss2015/ssBabies/v6.02-fs/T5QQQQVV_1200to1275_1to1150.root");
   TChain *chain = new TChain("t"); 
-  chain->Add("/nfs-7/userdata/ss2015/ssBabies/v6.02-fs/T5QQQQVV_*_*.root"); 
+  chain->Add("/nfs-7/userdata/ss2015/ssBabies/v6.02-fs/T1TTTT_*_*.root"); 
   TObjArray *listOfFiles = chain->GetListOfFiles();
   TIter fileIter(listOfFiles);
   TFile *currentFile = 0;
