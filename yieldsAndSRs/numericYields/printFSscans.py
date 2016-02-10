084818342b8a30446dfa2a74d3dc917751a535fa
Giuseppe <giuseppe.cerati@cern.ch>
2016-02-10 17:10:55 +0100

    protect against bugs on scale1fb

--- yieldsAndSRs/numericYields/printFSscans.py
+++ yieldsAndSRs/numericYields/printFSscans.py
@@ -3,6 +3,7 @@ import os, glob, ROOT
 scan = "T1TTTT"
 #scan = "T6TTWW_50"
 #scan = "T5QQQQVV"
+#scan = "T5QQQQWW"
 
 files = glob.glob("/nfs-7/userdata/ss2015/ssBabies/v6.02-fs/"+scan+"*.root")
 
@@ -11,15 +12,21 @@ outf = open("fs_%s.h" % scan.lower(), 'w')
 for fn in files:
     #print fn
     f = ROOT.TFile.Open(fn, "read")
+    t = f.Get("t")
     for key in f.GetListOfKeys():
         kname = key.GetName()
         #print kname
         if "csErr" in kname:
             masses = kname.split('_')
             #print masses[1], masses[2]
+            h = ROOT.TH1F("h_%s_%s"%(masses[1], masses[2]),"h_%s_%s"%(masses[1], masses[2]),2,-1,1)
+            t.Draw("scale1fb>>h_%s_%s"%(masses[1], masses[2]),"sparms[0]==%s && sparms[1]==%s" % (masses[1], masses[2]),"goff")
+            if h.GetMean()<0: 
+                print "negative average weight for mass point %s, %s" % (masses[1], masses[2])
+                continue
             outf.write("TChain *fs_%s_%s_%s_chain = new TChain(\"t\",\"fs_%s_m%s_m%s\" );\n" % (scan.lower(), masses[1], masses[2], scan.lower(), masses[1], masses[2]))
             outf.write("fs_%s_%s_%s_chain->Add(\"%s\");\n" % (scan.lower(), masses[1], masses[2], fn))
             outf.write("pair<yields_t, plots_t> results_fs_%s_%s_%s = run(fs_%s_%s_%s_chain, 0, 0, 0, 0, 1);\n" % (scan.lower(), masses[1], masses[2], scan.lower(), masses[1], masses[2]))
             outf.write("delete fs_%s_%s_%s_chain;\n\n" % (scan.lower(), masses[1], masses[2]))
-
+    f.Close()
 outf.close()
