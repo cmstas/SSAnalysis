e5bb457d788034acb979adc360e1b5ea0a33915f
Nick Amin <amin.nj@gmail.com>
2016-07-20 16:53:24 -0700

    add T5ttcc nPoints

--- cards/worker.sh
+++ cards/worker.sh
@@ -6,8 +6,18 @@ lumi=$5
 
 cd $where
 
-combine -M HybridNew --frequentist --testStat LHC -H ProfileLikelihood --seed -1 -T 500 -m ${cut} ${pfx}${cut}${postfix}/card_sig_${lumi}ifb-hihi.txt  -v 0 --rAbsAcc=0.05 --rRelAcc=0.015
+# # orig
+# Ntoys=500
+# AbsAcc=0.05
+# RelAcc=0.015
+
+# 3-4x longer, but more consistent values
+Ntoys=1500
+AbsAcc=0.025
+RelAcc=0.01
+
+combine -M HybridNew --frequentist --testStat LHC -H ProfileLikelihood --seed -1 -T $Ntoys -m ${cut} ${pfx}${cut}${postfix}/card_sig_${lumi}ifb-hihi.txt  -v 0 --rAbsAcc=$AbsAcc --rRelAcc=$RelAcc
 for q in 0.025 0.16 0.5 0.84 0.975; do 
-    combine -M HybridNew --frequentist --testStat LHC -H ProfileLikelihood --seed -1 -T 500 -m ${cut} ${pfx}${cut}${postfix}/card_sig_${lumi}ifb-hihi.txt  -v 0 --rAbsAcc=0.05 --rRelAcc=0.015 --expectedFromGrid=${q}
+    combine -M HybridNew --frequentist --testStat LHC -H ProfileLikelihood --seed -1 -T $Ntoys -m ${cut} ${pfx}${cut}${postfix}/card_sig_${lumi}ifb-hihi.txt  -v 0 --rAbsAcc=$AbsAcc --rRelAcc=$RelAcc --expectedFromGrid=${q}
 done
 wait
