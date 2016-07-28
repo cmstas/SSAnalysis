4b16d227f90c57e50946cb5b2592557460aaa1a0
Nick Amin <amin.nj@gmail.com>
2016-07-28 14:43:58 -0700

    fix

--- cards/makeMIlimitsnick.py
+++ cards/makeMIlimitsnick.py
@@ -2,22 +2,18 @@ import ROOT, array
 import sys, glob
 
 #### EDIT ME
-dir="MIlimits_new_6p26"
-lumi=6.26
+dir="MIlimits_12p9"
+lumi=12.9
 #### END EDIT
 
 met=[300,400,500,600,700,800,900,1000]
 ht=[1125,1200,1300,1400,1500,1600,1700,1800,1900,2000]
 
+magic = 5
+
+doMET = False
 doHT = True
 
-obs=[]
-exp=[]
-m2s=[]
-m1s=[]
-p1s=[]
-p2s=[]
-x0e=[]
 
 get_cut = lambda x: int(x.split(".mH")[1].split(".")[0])
 
@@ -33,167 +29,177 @@ files_p1s = sorted([f for f in met_files if "quant0.840" in f], key=get_cut)
 files_p2s = sorted([f for f in met_files if "quant0.975" in f], key=get_cut)
 
 
-for fn in files_obs:
-    f = ROOT.TFile.Open(fn)
-    for entry in f.limit:
-        #print entry.limit/lumi
-        obs.append(entry.limit/lumi)  
-        x0e.append(0.)
-        
-for fn in files_exp:
-    f = ROOT.TFile.Open(fn)
-    for entry in f.limit:
-        #print entry.limit/lumi
-        exp.append(entry.limit/lumi)  
+if doMET:
 
-for fn in files_m2s:
-    f = ROOT.TFile.Open(fn)
-    for entry in f.limit:
-        #print entry.limit/lumi
-        m2s.append(entry.limit/lumi)  
+    obs=[]
+    exp=[]
+    m2s=[]
+    m1s=[]
+    p1s=[]
+    p2s=[]
+    x0e=[]
 
-for fn in files_m1s:
-    f = ROOT.TFile.Open(fn)
-    for entry in f.limit:
-        #print entry.limit/lumi
-        m1s.append(entry.limit/lumi)  
+    for fn in files_obs:
+        f = ROOT.TFile.Open(fn)
+        for entry in f.limit:
+            #print entry.limit/lumi
+            obs.append(entry.limit/lumi)  
+            x0e.append(0.)
+            
+    for fn in files_exp:
+        f = ROOT.TFile.Open(fn)
+        for entry in f.limit:
+            #print entry.limit/lumi
+            exp.append(entry.limit/lumi)  
 
-for fn in files_p1s:
-    f = ROOT.TFile.Open(fn)
-    for entry in f.limit:
-        #print entry.limit/lumi
-        p1s.append(entry.limit/lumi)  
+    for fn in files_m2s:
+        f = ROOT.TFile.Open(fn)
+        for entry in f.limit:
+            #print entry.limit/lumi
+            m2s.append(entry.limit/lumi)  
 
-for fn in files_p2s:
-    f = ROOT.TFile.Open(fn)
-    for entry in f.limit:
-        #print entry.limit/lumi
-        p2s.append(entry.limit/lumi)  
+    for fn in files_m1s:
+        f = ROOT.TFile.Open(fn)
+        for entry in f.limit:
+            #print entry.limit/lumi
+            m1s.append(entry.limit/lumi)  
 
-print p2s
-print p1s
-print exp
-print m1s
-print m2s
-print obs
+    for fn in files_p1s:
+        f = ROOT.TFile.Open(fn)
+        for entry in f.limit:
+            #print entry.limit/lumi
+            p1s.append(entry.limit/lumi)  
 
-#TGraphAsymmErrors needs the delta
-for i in range(0,len(exp)):
-    m2s[i]=exp[i]-m2s[i]
-    m1s[i]=exp[i]-m1s[i]
-    p1s[i]=p1s[i]-exp[i]
-    p2s[i]=p2s[i]-exp[i]
+    for fn in files_p2s:
+        f = ROOT.TFile.Open(fn)
+        for entry in f.limit:
+            #print entry.limit/lumi
+            p2s.append(entry.limit/lumi)  
 
-ROOT.gStyle.SetOptStat(0)
-c1 = ROOT.TCanvas("c1", "", 800, 800)
-c1.cd()
-padt = ROOT.TPad("p_tex", "p_tex", 0.0, 0.0, 1.0, 1.0)
-padt.SetTopMargin(0.1)
-padt.SetBottomMargin(0.1)
-padt.SetRightMargin(0.05)
-padt.SetLeftMargin(0.15)
-padt.SetTickx()
-padt.SetTicky()
-padt.Draw()
-padt.cd()
+    print p2s
+    print p1s
+    print exp
+    print m1s
+    print m2s
+    print obs
 
-h = ROOT.TH2F("h","h",16,250,1050,5,0,5)
-h.SetTitle("")
-h.GetXaxis().SetTitle("E_{T}^{miss} (GeV)")
-h.GetYaxis().SetTitle("#sigma#kern[0.1]{A}#kern[0.1]{#varepsilon} limit at 95% CL (fb)")
-h.GetXaxis().SetLabelSize(0.04)
-h.GetYaxis().SetLabelSize(0.04)
-h.GetXaxis().SetTitleSize(0.045)
-h.GetYaxis().SetTitleSize(0.045)
-h.GetXaxis().SetTitleOffset(1.0)
-h.GetYaxis().SetTitleOffset(1.5)
-h.Draw()
+    #TGraphAsymmErrors needs the delta
+    for i in range(0,len(exp)):
+        m2s[i]=exp[i]-m2s[i]
+        m1s[i]=exp[i]-m1s[i]
+        p1s[i]=p1s[i]-exp[i]
+        p2s[i]=p2s[i]-exp[i]
 
-gr_s2b = ROOT.TGraphAsymmErrors(len(met),array.array('d', met),array.array('d', exp),array.array('d', x0e),array.array('d', x0e),array.array('d', m2s),array.array('d', p2s))
-gr_s2b.SetFillColor(ROOT.kYellow)
-gr_s2b.Draw("3")
-  
-gr_s1b = ROOT.TGraphAsymmErrors(len(met),array.array('d', met),array.array('d', exp),array.array('d', x0e),array.array('d', x0e),array.array('d', m1s),array.array('d', p1s))
-gr_s1b.SetFillColor(ROOT.kGreen)
-gr_s1b.Draw("3")
+    ROOT.gStyle.SetOptStat(0)
+    c1 = ROOT.TCanvas("c1", "", 800, 800)
+    c1.cd()
+    padt = ROOT.TPad("p_tex", "p_tex", 0.0, 0.0, 1.0, 1.0)
+    padt.SetTopMargin(0.1)
+    padt.SetBottomMargin(0.1)
+    padt.SetRightMargin(0.05)
+    padt.SetLeftMargin(0.15)
+    padt.SetTickx()
+    padt.SetTicky()
+    padt.Draw()
+    padt.cd()
 
-gexp = ROOT.TGraph(len(met), array.array('d', met), array.array('d', exp))
-gexp.SetLineStyle(7)
-gexp.SetLineWidth(3)
-gexp.SetLineColor(ROOT.kBlack)
-gexp.Draw("L")
+    h = ROOT.TH2F("h","h",16,250,1050,magic,0,magic)
+    h.SetTitle("")
+    h.GetXaxis().SetTitle("E_{T}^{miss} (GeV)")
+    h.GetYaxis().SetTitle("#sigma#kern[0.1]{A}#kern[0.1]{#varepsilon} limit at 95% CL (fb)")
+    h.GetXaxis().SetLabelSize(0.04)
+    h.GetYaxis().SetLabelSize(0.04)
+    h.GetXaxis().SetTitleSize(0.045)
+    h.GetYaxis().SetTitleSize(0.045)
+    h.GetXaxis().SetTitleOffset(1.0)
+    h.GetYaxis().SetTitleOffset(1.5)
+    h.Draw()
 
-gobs = ROOT.TGraph(len(met), array.array('d', met), array.array('d', obs))
-gobs.SetMarkerStyle(ROOT.kFullCircle)
-gobs.SetMarkerSize(1.5)
-gobs.SetMarkerColor(ROOT.kBlack)
-gobs.SetLineWidth(3)
-gobs.SetLineColor(ROOT.kBlack)
-gobs.Draw("PL")
+    gr_s2b = ROOT.TGraphAsymmErrors(len(met),array.array('d', met),array.array('d', exp),array.array('d', x0e),array.array('d', x0e),array.array('d', m2s),array.array('d', p2s))
+    gr_s2b.SetFillColor(ROOT.kYellow)
+    gr_s2b.Draw("3")
+      
+    gr_s1b = ROOT.TGraphAsymmErrors(len(met),array.array('d', met),array.array('d', exp),array.array('d', x0e),array.array('d', x0e),array.array('d', m1s),array.array('d', p1s))
+    gr_s1b.SetFillColor(ROOT.kGreen)
+    gr_s1b.Draw("3")
 
-cmstex = ROOT.TLatex(0.675,0.91, "%.2f fb^{-1} (13 TeV)" % lumi)
-cmstex.SetNDC()
-cmstex.SetTextSize(0.04)
-cmstex.SetLineWidth(2)
-cmstex.SetTextFont(42)
-cmstex.Draw()
+    gexp = ROOT.TGraph(len(met), array.array('d', met), array.array('d', exp))
+    gexp.SetLineStyle(7)
+    gexp.SetLineWidth(3)
+    gexp.SetLineColor(ROOT.kBlack)
+    gexp.Draw("L")
 
-cmstexbold = ROOT.TLatex(0.17,0.91, "CMS" )
-cmstexbold.SetNDC()
-cmstexbold.SetTextSize(0.05)
-cmstexbold.SetLineWidth(2)
-cmstexbold.SetTextFont(61)
-cmstexbold.Draw()
+    gobs = ROOT.TGraph(len(met), array.array('d', met), array.array('d', obs))
+    gobs.SetMarkerStyle(ROOT.kFullCircle)
+    gobs.SetMarkerSize(1.5)
+    gobs.SetMarkerColor(ROOT.kBlack)
+    gobs.SetLineWidth(3)
+    gobs.SetLineColor(ROOT.kBlack)
+    gobs.Draw("PL")
 
-cmstexprel = ROOT.TLatex(0.29,0.91, "Preliminary" )
-cmstexprel.SetNDC()
-cmstexprel.SetTextSize(0.03)
-cmstexprel.SetLineWidth(2)
-cmstexprel.SetTextFont(52)
-cmstexprel.Draw()
+    cmstex = ROOT.TLatex(0.675,0.91, "%.2f fb^{-1} (13 TeV)" % lumi)
+    cmstex.SetNDC()
+    cmstex.SetTextSize(0.04)
+    cmstex.SetLineWidth(2)
+    cmstex.SetTextFont(42)
+    cmstex.Draw()
 
-l1 = ROOT.TLegend(0.15, 0.70, 0.95, 0.90)
-l1.SetTextFont(42)
-l1.SetTextSize(0.036)
-l1.SetShadowColor(ROOT.kWhite)
-l1.SetFillColor(ROOT.kWhite)
-l1.SetHeader("Model independent #sigma#kern[0.1]{A}#kern[0.1]{#varepsilon} exclusion limit")
-l1.AddEntry(gobs , "Observed", "lp")
-l1.AddEntry(gexp , "Expected #pm 1 #sigma #pm 2 #sigma", "l")
-l1.Draw()
+    cmstexbold = ROOT.TLatex(0.17,0.91, "CMS" )
+    cmstexbold.SetNDC()
+    cmstexbold.SetTextSize(0.05)
+    cmstexbold.SetLineWidth(2)
+    cmstexbold.SetTextFont(61)
+    cmstexbold.Draw()
 
-LExp1 = ROOT.TGraphAsymmErrors(2)
-LExp1.SetFillColor(ROOT.kYellow)
-LExp1.SetPoint(0,250+ 3.8*(1050-250)/100, 5-2.08*(5-0)/100*10)
-LExp1.SetPointError(0,0.,0.,0.15,0.15)
-LExp1.SetPoint(1,250+21.2*(1050-250)/100, 5-2.08*(5-0)/100*10)
-LExp1.SetPointError(1,0.,0.,0.15,0.15)
-LExp1.Draw("3")
+    cmstexprel = ROOT.TLatex(0.29,0.91, "Preliminary" )
+    cmstexprel.SetNDC()
+    cmstexprel.SetTextSize(0.03)
+    cmstexprel.SetLineWidth(2)
+    cmstexprel.SetTextFont(52)
+    cmstexprel.Draw()
 
-LExp2 = ROOT.TGraphAsymmErrors(2)
-LExp2.SetFillColor(ROOT.kGreen)
-LExp2.SetPoint(0,250+ 3.8*(1050-250)/100, 5-2.08*(5-0)/100*10)
-LExp2.SetPointError(0,0.,0.,0.08,0.08)
-LExp2.SetPoint(1,250+21.2*(1050-250)/100, 5-2.08*(5-0)/100*10)
-LExp2.SetPointError(1,0.,0.,0.08,0.08)
-LExp2.Draw("L3")
+    l1 = ROOT.TLegend(0.15, 0.70, 0.95, 0.90)
+    l1.SetTextFont(42)
+    l1.SetTextSize(0.036)
+    l1.SetShadowColor(ROOT.kWhite)
+    l1.SetFillColor(ROOT.kWhite)
+    l1.SetHeader("Model independent #sigma#kern[0.1]{A}#kern[0.1]{#varepsilon} exclusion limit")
+    l1.AddEntry(gobs , "Observed", "lp")
+    l1.AddEntry(gexp , "Expected #pm 1 #sigma #pm 2 #sigma", "l")
+    l1.Draw()
 
-LExp = ROOT.TGraph(2)
-LExp.SetLineColor(ROOT.kBlack)
-LExp.SetLineStyle(7)
-LExp.SetLineWidth(3)
-LExp.SetPoint(0,250+ 3.8*(1050-250)/100, 5-2.08*(5-0)/100*10)
-LExp.SetPoint(1,250+21.2*(1050-250)/100, 5-2.08*(5-0)/100*10)
-LExp.Draw("L")
+    LExp1 = ROOT.TGraphAsymmErrors(2)
+    LExp1.SetFillColor(ROOT.kYellow)
+    LExp1.SetPoint(0,250+ 3.8*(1050-250)/100, magic-2.08*(magic-0)/100*10)
+    LExp1.SetPointError(0,0.,0.,0.15,0.15)
+    LExp1.SetPoint(1,250+21.2*(1050-250)/100, magic-2.08*(magic-0)/100*10)
+    LExp1.SetPointError(1,0.,0.,0.15,0.15)
+    LExp1.Draw("3")
 
-masstex = ROOT.TLatex(0.70,0.79, "H_{T} > 300 GeV" )
-masstex.SetNDC()
-masstex.SetTextSize(0.036)
-masstex.SetLineWidth(2)
-masstex.SetTextFont(42)
-masstex.Draw()
+    LExp2 = ROOT.TGraphAsymmErrors(2)
+    LExp2.SetFillColor(ROOT.kGreen)
+    LExp2.SetPoint(0,250+ 3.8*(1050-250)/100, magic-2.08*(magic-0)/100*10)
+    LExp2.SetPointError(0,0.,0.,0.08,0.08)
+    LExp2.SetPoint(1,250+21.2*(1050-250)/100, magic-2.08*(magic-0)/100*10)
+    LExp2.SetPointError(1,0.,0.,0.08,0.08)
+    LExp2.Draw("L3")
 
-c1.SaveAs("MET_MIXSEC_%s.pdf" % (str(lumi).replace(".","p")))
+    LExp = ROOT.TGraph(2)
+    LExp.SetLineColor(ROOT.kBlack)
+    LExp.SetLineStyle(7)
+    LExp.SetLineWidth(3)
+    LExp.SetPoint(0,250+ 3.8*(1050-250)/100, magic-2.08*(magic-0)/100*10)
+    LExp.SetPoint(1,250+21.2*(1050-250)/100, magic-2.08*(magic-0)/100*10)
+    LExp.Draw("L")
+
+    masstex = ROOT.TLatex(0.70,0.79, "H_{T} > 300 GeV" )
+    masstex.SetNDC()
+    masstex.SetTextSize(0.036)
+    masstex.SetLineWidth(2)
+    masstex.SetTextFont(42)
+    masstex.Draw()
+
+    c1.SaveAs("MET_MIXSEC_%s.pdf" % (str(lumi).replace(".","p")))
 
 #####################################################################################################
 
@@ -217,42 +223,46 @@ if doHT:
     print files_obs
 
     for fn in files_obs:
-        f = ROOT.TFile.Open(fn)
-        for entry in f.limit:
-            #print entry.limit/lumi
-            print f,entry.limit, lumi, entry.limit/lumi
-            obs.append(entry.limit/lumi)  
-            x0e.append(0.)
+            f = ROOT.TFile.Open(fn)
+        # try:
+            print fn
+            for entry in f.limit:
+                #print entry.limit/lumi
+                obs.append(entry.limit/lumi)  
+                x0e.append(0.)
+        # except:
+        #     obs.append(0.0)
+        #     x0e.append(0.)
 
     for fn in files_exp:
-        f = ROOT.TFile.Open(fn)
-        for entry in f.limit:
-            #print entry.limit/lumi
-            exp.append(entry.limit/lumi)  
+            f = ROOT.TFile.Open(fn)
+        # try:
+            for entry in f.limit: exp.append(entry.limit/lumi)  
+        # except: exp.append(0.0)  
 
     for fn in files_m2s:
-        f = ROOT.TFile.Open(fn)
-        for entry in f.limit:
-            # print entry.limit/lumi, entry.limitErr/entry.limit/lumi
-            m2s.append(entry.limit/lumi)  
+            f = ROOT.TFile.Open(fn)
+        # try:
+            for entry in f.limit: m2s.append(entry.limit/lumi)  
+        # except: m2s.append(0.0)  
 
     for fn in files_m1s:
-        f = ROOT.TFile.Open(fn)
-        for entry in f.limit:
-            #print entry.limit/lumi
-            m1s.append(entry.limit/lumi)  
+            f = ROOT.TFile.Open(fn)
+        # try:
+            for entry in f.limit: m1s.append(entry.limit/lumi)  
+        # except: m1s.append(0.0)  
 
     for fn in files_p1s:
-        f = ROOT.TFile.Open(fn)
-        for entry in f.limit:
-            #print entry.limit/lumi
-            p1s.append(entry.limit/lumi)  
+            f = ROOT.TFile.Open(fn)
+        # try:
+            for entry in f.limit: p1s.append(entry.limit/lumi)  
+        # except: p1s.append(0.0)  
 
     for fn in files_p2s:
-        f = ROOT.TFile.Open(fn)
-        for entry in f.limit:
-            #print entry.limit/lumi
-            p2s.append(entry.limit/lumi)  
+            f = ROOT.TFile.Open(fn)
+        # try:
+            for entry in f.limit: p2s.append(entry.limit/lumi)  
+        # except: p2s.append(0.0)  
 
     print p2s
     print p1s
