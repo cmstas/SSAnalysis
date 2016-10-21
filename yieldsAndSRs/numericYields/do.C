2c5aa61856ddcdbec3f6c24bbce2fcd0836a37a0
Nick Amin <amin.nj@gmail.com>
2016-10-21 13:48:28 -0700

    remove useless lines

--- yieldsAndSRs/numericYields/do.C
+++ yieldsAndSRs/numericYields/do.C
@@ -1,10 +1,8 @@
 {
-  // gROOT->ProcessLine(".L ../../classFiles/v7.00/SS.cc+"); 
   gROOT->ProcessLine(".L ../../classFiles/v8.02/SS.cc+"); 
   gSystem->Load("../../CORE/CMS3_CORE.so");
   gROOT->ProcessLine(".L ../../CORE/Tools/dorky/dorky.cc+");
   gROOT->ProcessLine(".L ../../software/dataMCplotMaker/dataMCplotMaker.cc+"); 
-  // gROOT->ProcessLine(".L ../../hlt/HLTEfficiency.cc+"); 
   gSystem->Load("../../software/tableMaker/libSimpleTable.so"); 
   gROOT->ProcessLine(".L yieldMaker.C++"); 
   gROOT->ProcessLine("getyields()"); 
