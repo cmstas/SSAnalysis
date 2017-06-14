b896f659fd9ea7f6aee68ac0702784b5ad0bfad5
Philip Chang <sgnoohc@gmail.com>
2017-06-14 16:05:48 -0700

    Add a header file for ScanChain

--- FakeRate/measurement_region/ScanChain.C
+++ FakeRate/measurement_region/ScanChain.C
@@ -1,38 +1,4 @@
-// Usage:
-// > root -b doAll.C
-
-// C++
-#include <iostream>
-#include <iomanip>
-#include <vector>
-
-// ROOT
-#include "TBenchmark.h"
-#include "TChain.h"
-#include "TDirectory.h"
-#include "TFile.h"
-#include "TROOT.h"
-#include "TTreeCache.h"
-#include "TH2D.h"
-#include "TCanvas.h"
-#include "TStyle.h"
-#include "Math/VectorUtil.h"
-#include <unistd.h> //isatty
-
-// lepfilter
-#include "LeptonTree.cc"
-
-#include "../../CORE/Tools/JetCorrector.cc"
-#include "../../commonUtils.h"
-#include "vtx_reweight.h"
-
-using namespace std;
-using namespace lepton_tree;
-
-#ifdef __MAKECINT__
-#pragma link C++ class ROOT::Math::PxPyPzE4D<float>+;
-#pragma link C++ class ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> >+;
-#endif
+#include "ScanChain.h"
 
 float computePtRel(LorentzVector lepp4, LorentzVector jetp4, bool subtractLep) {
   if (jetp4.pt()==0) return 0.;
